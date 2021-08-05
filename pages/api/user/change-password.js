a3dbab2d053df405c98ce209c8d4e1ade4d1e1eb
Eric Einhaus <eeinhaus17@gmail.com>
2021-08-05 14:54:34 -0400

    change password api route added

--- pages/api/user/change-password.js
+++ pages/api/user/change-password.js
@@ -1,4 +1,7 @@
 import { getSession } from 'next-auth/client';
+import { connectDB } from '../../../helpers/db';
+import { verifyPassword, saltAndHash } from '../../../helpers/auth';
+import { passwordRegex } from '../../../helpers/regexs';
 
 async function handler(req, res) {
   if (req.method !== 'PATCH') {
@@ -13,5 +16,49 @@ async function handler(req, res) {
       .json({ message: 'Security error: Unable to reset password' });
     return;
   }
+
+  const userEmail = session.user.email;
+  const oldPassword = req.body.oldPassword;
+  const newPassword = req.body.newPassword;
+
+  const isPasswordValid = passwordRegex.test(newPassword);
+  if (!isPasswordValid) {
+    res
+      .status(422)
+      .json({ message: 'New password must meet requirements, try again.' });
+    return;
+  }
+
+  const client = await connectDB();
+  const usersCollection = client.db().collection('users');
+
+  const user = await usersCollection.findOne({ email: userEmail });
+
+  if (!user) {
+    client.close();
+    res.status(404).json({ message: 'User not found' });
+    return;
+  }
+
+  const currentPassword = user.password;
+  const arePasswordsEqual = await verifyPassword(oldPassword, currentPassword);
+
+  if (!arePasswordsEqual) {
+    client.close();
+    res
+      .status(403) //authorized, but not authenticated
+      .json({ message: 'Security error: Unable to reset password' });
+    return;
+  }
+
+  const encryptedPassword = await saltAndHash(newPassword);
+
+  await usersCollection.updateOne(
+    { email: userEmail },
+    { $set: { password: encryptedPassword } }
+  );
+
+  client.close();
+  res.status(200).json({ message: 'Password successfully updated!' });
 }
 export default handler;
