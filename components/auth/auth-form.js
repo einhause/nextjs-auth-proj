8d2cec18f4eb39bda5e2fbdca7b75227f9fa0113
Eric Einhaus <eeinhaus17@gmail.com>
2021-08-05 13:17:41 -0400

    Connected client side with next auth using signIn method

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,4 +1,5 @@
 import { useState, useRef } from 'react';
+import { signIn } from 'next-auth/client';
 import { createUser } from '../../helpers/auth';
 import classes from './auth-form.module.css';
 
@@ -20,7 +21,15 @@ function AuthForm() {
     let resultData;
 
     if (isLogin) {
-      // login
+      resultData = await signIn('credentials', {
+        redirect: false,
+        email: enteredEmail,
+        password: enteredPass,
+      });
+
+      if (!resultData.error) {
+        // set auth state
+      }
     } else {
       resultData = await createUser(enteredEmail, enteredPass);
     }
